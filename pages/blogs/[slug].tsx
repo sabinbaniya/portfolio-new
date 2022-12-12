1d549f8f520113f786119e68b967d8ca57aeed15
sabinbaniya <dummymail@tutanota.de>
2022-12-12 21:53:36 +0545

    blogwip

---
+++ pages/blogs/[slug].tsx
@@ -0,0 +1,35 @@
+import Head from "next/head";
+import ReactMarkdown from "react-markdown";
+import rehypeRaw from "rehype-raw";
+import medium_post from "../../public/medium_post_response.json";
+import Layout from "../../src/components/Layout";
+import { BlogPosts } from "../../src/types";
+interface Props {}
+
+const Slugs = ({}: Props) => {
+  return (
+    <>
+      <Head>
+        <title>{medium_post.items[0].title}</title>
+      </Head>
+      <Layout blogPosts={[] as BlogPosts[]}>
+        <section className="max-w-3xl mx-auto mt-28">
+          <div className="text-4xl leading-relaxed font-bold mb-8">
+            <ReactMarkdown rehypePlugins={[rehypeRaw]}>
+              {medium_post.items[0].title}
+            </ReactMarkdown>
+          </div>
+          <div id="blog-post-body">
+            <ReactMarkdown rehypePlugins={[rehypeRaw]}>
+              {medium_post.items[0].content}
+            </ReactMarkdown>
+          </div>
+        </section>
+      </Layout>
+    </>
+  );
+};
+
+export default Slugs;
+
+// export default {};
